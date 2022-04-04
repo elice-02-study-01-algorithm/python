e2d7f3b5444e0893db4467b73363d9afe22c1570
[4기-1반] 김채정 <whenucan35@gmail.com>
2022-04-05 01:35:43 +0900

    [baekjoon-4881]자리수의제곱.py

--- CJ_Kim/1주차/[Baekjoon-4881]자리수의제곱.py
+++ CJ_Kim/1주차/[Baekjoon-4881]자리수의제곱.py
@@ -1,19 +1,36 @@
-# from itertools import combinations
-# numList = [i for i in range(1, 81)]
+def makeSquareSum(num):
+    numList = [num]
+    while numList.count(numList[-1]) <2:
+        calNum = numList[-1]
+        if calNum <10:
+            numList.append(calNum**2)
+        else:
+            newNum = 0
+            for i in str(calNum):
+                newNum += int(i)**2
+            numList.append(newNum)
+    return numList
 
-# newNumList = []
-# for i in numList:
-#     newNumList.append(i*i)
-# resultList = list(combinations(newNumList ,2))
+def existSameNum(list01, list02):
+    minNum = 0
+    for i in range(len(list01)):
+        for j in range(len(list02)):
+            if list01[i] == list02[j]:
+                minNum = i+j+2
+    for j in range(len(list02)):
+        for i in range(len(list01)):
+            if list01[i] == list02[j]:
+                if minNum > i+j+2:
+                    minNum = i+j+2
 
-# finalList = []
-# for n in resultList:
-#     x, y = n
-#     finalList.append(x+y)
-# print(sorted(finalList))
+    return minNum
 
-def makeDoubleSum(num):
-    numList = []
-    
-    for i in str(num):
-        numList.append(int(i)^2)
+while True:
+    numA, numB = map(int, input().split())
+    if numA == 0 and numB == 0:
+        break
+    else:
+        numList01 = makeSquareSum(numA)
+        numList02 = makeSquareSum(numB)
+        print(numA, numB, existSameNum(numList01, numList02))
+        
