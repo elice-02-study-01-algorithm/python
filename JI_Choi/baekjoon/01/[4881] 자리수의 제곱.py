47a28c9c2029b7c9474e4493d3d968341908fc1b
@eikjae <eikjae@gmail.com>
2022-04-06 12:04:37 +0900

    [Baekjoon-4881] 자리수의 제곱 (주석 추가)

--- JI_Choi/baekjoon/01/[4881] 자리수의 제곱.py
+++ JI_Choi/baekjoon/01/[4881] 자리수의 제곱.py
@@ -6,7 +6,7 @@ while True:
   c = [a, b]
   data.append(c)
 
-# 다음 수열 구하기
+# 다음 수열을 구하는 함수
 def nextSeq(num):
   splitString = list(str(num))
   splitNum = list(map(int, splitString))
@@ -17,7 +17,7 @@ def nextSeq(num):
 
 ### 최솟값 구하기 ###
 for elem in data:
-  
+  # 순환하기 직전의 수열까지 구하기
   x = elem[0]
   list1 = [x]
   while True:
@@ -27,6 +27,7 @@ for elem in data:
     else:
       break
 
+  # 순환하기 직전의 수열까지 구하기
   y = elem[1]
   list2 = [y]
   while True:
@@ -36,15 +37,17 @@ for elem in data:
     else:
       break
 
-  # 경우 나누기
+  # 1. 리스트의 마지막 원소가 각각 1과 1이 아닌 자연수인 경우
   if list1[-1] == 1 and list2[-1] != 1:
     print(list1[0], list2[0], 0)
 
   elif list1[-1] != 1 and list2[-1] == 1:
     print(list1[0], list2[0], 0)
   
+  # 2. 그 외의 경우 
   else:
     pseudoAnswers = []
+    # 유사 정답들을 리스트에 모으고, 정답(가장 작은 값)을 출력 
     for i in range(len(list1)):
       if list1[i] in list2:
         pseudoAnswer = i + list2.index(list1[i]) + 2
