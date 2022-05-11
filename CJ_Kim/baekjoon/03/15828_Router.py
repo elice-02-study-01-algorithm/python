87f1f3cce2d50581c8b7655a50925e04af380312
[4기-1반] 김채정 <whenucan35@gmail.com>
2022-05-12 08:53:31 +0900

    [Baekjoon-15828] Router 주석 추가

--- CJ_Kim/baekjoon/03/15828_Router.py
+++ CJ_Kim/baekjoon/03/15828_Router.py
@@ -1,5 +1,6 @@
 # 50점짜리
 '''
+# 큐문제라서 당연히 큐로 시도해보았음
 from queue import Queue
 buffer = int(input())
 bufferQ = Queue()
@@ -10,6 +11,7 @@ while True:
         break
     elif info == 0:
         bufferQ.get()
+    # 버퍼의 크기보다 정보가 적게 있을 때만 위치시키기
     elif bufferQ.qsize()<buffer:
         bufferQ.put(info)
 if bufferQ.qsize()==0:
@@ -17,11 +19,18 @@ if bufferQ.qsize()==0:
 else:
     while bufferQ.qsize()>0:
         print(bufferQ.get())
+
+# 아래의 코드와 로직은 다를 게 없어보이는 데 
+# 아마 나머지 50점은 시간초과로 추측됨
 '''
 # 100점 짜리
+
+# 50점을 맞기도 했고, 
+# 채점 중 퍼센트가 매우 느리게 올라가는 것을 확인하여 재시도
 import sys
 
 buffer = int(sys.stdin.readline())
+# 큐를 리스트로 구현
 bufferList = []
 
 while True:
