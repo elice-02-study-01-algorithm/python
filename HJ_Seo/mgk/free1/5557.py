76809b65a4b938c46b7b21984ebcc492cc1605c0
seogudwns <38065726+seogudwns@users.noreply.github.com>
2022-05-29 12:28:34 +0900

    1주차 모각코(설명 추가)

--- HJ_Seo/mgk/free1/5557.py
+++ HJ_Seo/mgk/free1/5557.py
@@ -2,9 +2,11 @@ from sys import stdin
 
 n = int(stdin.readline().strip())
 nums = tuple(map(int,stdin.readline().strip().split()))
-# print(n,nums)
+
 all_rst = {nums[0]:1}
+#초기 결과. 처음에는 수와 그 횟수가 유일함.
 temp = dict()
+# next_result를 받아줄 dictionary.
 
 for i in range(1,n-1):
     for j in all_rst:
@@ -13,11 +15,13 @@ for i in range(1,n-1):
             if j - nums[i] not in temp:
                 temp[j - nums[i]] = 0
             temp[j - nums[i]] += all_rst[j]
+            # 이전 상황에서 all_rst[j] 만큼의 j가 나온 상태. 따라서 j- num[i] 는 all_rst[j]만큼 추가됨.
             
         if j + nums[i] <= 20:
             if j + nums[i] not in temp:
                 temp[j + nums[i]] = 0
             temp[j + nums[i]] += all_rst[j]
+            #위와 마찬가지.
 
     all_rst = temp
     # print(all_rst)
