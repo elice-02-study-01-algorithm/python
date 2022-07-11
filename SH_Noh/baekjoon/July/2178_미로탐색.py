471a038b4ca9a32f1073b3e2b6224157dc3f719e
SongheeNoh <s0n9h2@gmail.com>
2022-07-12 02:05:26 +0900

    [baekjoon-2178] 미로 탐색 해결

--- SH_Noh/baekjoon/July/2178_미로탐색.py
+++ SH_Noh/baekjoon/July/2178_미로탐색.py
@@ -21,14 +21,12 @@ def bfs(x, y):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if 0 <= nx < N and 0 <= ny < N and maze[nx][ny] == 1:
-                # 아직 방문하지 않은 칸이거나 현재칸보다 한 칸 더 걸리는 칸
+            if 0 <= nx < N and 0 <= ny < M and maze[nx][ny] == 1:
+                # 아직 방문하지 않은 칸이거나 현재칸에서 한 칸 더한 것보다 더 걸리는 칸
                 if visited[nx][ny] == 0 or visited[nx][ny] > visited[x][y] + 1:
                     visited[nx][ny] = visited[x][y] + 1
                     q.append((nx, ny))
-                    print(q)
     
-    print(visited)
     return visited[N-1][M-1]
 
 print(bfs(0, 0))
