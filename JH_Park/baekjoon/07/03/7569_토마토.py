e445f9cec7531dd741de05ca047240d79d4b791b
Malza0408 <parkjh04080@gmail.com>
2022-07-19 21:25:27 +0900

    [Baekjoon-7569] 토마토

--- JH_Park/baekjoon/07/03/7569_토마토.py
+++ JH_Park/baekjoon/07/03/7569_토마토.py
@@ -9,40 +9,37 @@ for i in range(h):
         temp.append(list(map(int, input().split())))
         for k in range(m):
             if temp[j][k] == 1:
-                q.append([i, j, k])
+                q.append((k, j, i))
     boxes.append(temp)
-
 # 위아래 오른왼
-dir_x = [0, 0, 1, -1, 0, 0]
-dir_y = [-1, 1, 0, 0, 0, 0]
+dir_x = [1, 0, -1, 0, 0, 0]
+dir_y = [0, -1, 0, 1, 0, 0]
 dir_z = [0, 0, 0, 0, 1, -1]
 
 def bfs():
     while q:
         # 1인 좌표
-        cur = q.popleft()
+        cur_x, cur_y, cur_z = q.popleft()
         # 방향을 더해주면서
         for i in range(6):
-            cur_x = cur[0] + dir_x[i]
-            cur_y = cur[1] + dir_y[i]
-            cur_z = cur[2] + dir_z[i]
-        if cur_x < 0 or cur_y < 0 or cur_z < 0 or cur_x >= n or cur_y >= m or cur_z >= h:
-            return
-        if boxes[cur_z][cur_x][cur_y] == 0:
-            q.append([cur_z, cur_x, cur_y])
-            boxes[cur_z][cur_x][cur_y] = boxes[cur[0]][cur[1]][cur[2]] + 1
+            move_x = cur_x + dir_x[i]
+            move_y = cur_y + dir_y[i]
+            move_z = cur_z + dir_z[i]
+            if 0 <= move_x < m and 0 <= move_y < n and 0 <= move_z < h and boxes[move_z][move_y][move_x] == 0:
+                q.append((move_x, move_y, move_z))
+                boxes[move_z][move_y][move_x] = boxes[cur_z][cur_y][cur_x] + 1
             
 
 bfs()
+
 result = 0
-print(boxes)
 for i in boxes:
     for row in i:
           for col in row:
               if col == 0:
                   print(-1)
                   exit()
-              result = max(result, max(row))
+              # result = max(result, max(row))
+              if col > result:
+                  result = col
 print(result - 1)
-
-# 실패!
