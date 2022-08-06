db2fcae40468420c7745fb8bbfbd533815cf188e
seogudwns <38065726+seogudwns@users.noreply.github.com>
2022-08-06 12:58:02 +0900

    baekjoon private

--- HJ_Seo/prac.py
+++ HJ_Seo/prac.py
@@ -230,32 +230,12 @@ WANTED: 7977270
 
 # print(tuple({1,4,6})[2])
 
-n = input()
+print({1,2,5} & {1,2,3,4})
 
-checknum = 1
-while True:
-    n_change = ''
-    for i in n[:8]:
-        n_change += chr(ord(i) ^ checknum)
-    
-    if n_change == 'CHICKENS':
-        # print(checknum)
-        break
-    
-    checknum += 1
-    
-n_before = 'CHICKENS'
-for i in n[8:]:
-    n_before += chr(ord(i) ^ checknum)
+print(chr(97),chr(122))
 
-print(n_before)
+for i in range(97,123):
+    print(chr(i),i)
 
-# for i in 'CHI':
-#     print(bin(ord(i)))
-#     print(bin(ord(i)^22))
-#     print('===')
-# print(bin(22))
-
-# ! 이게 왜 정답이야???...
-
-for x in input():print(end=chr(ord(x)^22))
+# 116,111,115
+print(chr(116).upper())
