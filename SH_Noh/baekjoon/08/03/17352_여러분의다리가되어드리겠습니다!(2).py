5631e484d4e3ef7ceb465bbf45863e8c754e185c
SongheeNoh <s0n9h2@gmail.com>
2022-08-18 15:38:57 +0900

    [baekjoon-17352] 여러분의 다리가 되어 드리겠습니다!_bfs 버전

---
+++ SH_Noh/baekjoon/08/03/17352_여러분의다리가되어드리겠습니다!(2).py
@@ -0,0 +1,32 @@
+# 두 개로 나눠진 영역의 양 끝단 중 아무거나 이어주면 됨
+from collections import deque
+from sys import stdin
+input = stdin.readline
+
+def solution(graph, island):
+    q = deque([island])
+    visited = [0] * (N + 1)
+    while q:
+        i = q.popleft()
+        if visited[i] == 0:
+            q.extend(graph[i])
+        visited[i] += 1
+    return visited
+
+if __name__ == "__main__":
+    N = int(input())
+    graph = {x: [] for x in range(1, N + 1)}
+    for _ in range(N-2):
+        i1, i2 = map(int, input().split())
+        graph[i1].append(i2)
+        graph[i2].append(i1)
+
+    visited1 = solution(graph, 1)
+    a = visited1.index(1)
+    next = visited1.index(0, 1)
+    visited2 = solution(graph, next)
+    b = visited2.index(1)
+    # print(graph)
+    # print(visited1)
+    # print(visited2)
+    print(a, b)
